5a2cc9d960446d18a70d8a4cdd2b7ef46c7d9eaf
David Jurado <19822818+djurado@users.noreply.github.com>
2024-06-19 17:06:02 -0500

    Clase 02 - Componente TodoItem

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edita el archivo <code>src/App.js</code> y guarda para recargar.
         </p>
+        <p>Mi primer cambio</p>
         <a
           className="App-link"
           href="https://platzi.com/reactjs"
@@ -19,6 +23,15 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+function TodoItem(){
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar a la lloreria</p>
+      <span>X</span>
+    </li>
   );
 }
 
