69dfd9adbf4da628de443c7a78e42d61648cdc34
David Jurado <19822818+djurado@users.noreply.github.com>
2024-07-01 01:01:29 -0500

    Clase 09 - Buscando, compleetando y eliminando ToDos usando estados

--- src/App.js
+++ src/App.js
@@ -9,20 +9,44 @@ import React from 'react';
 
 const defaultTodos = [
   {text: 'Crear los temas del taller de FP', completed: false},
-  {text: 'Lavar los platos', completed: false},
+  {text: 'Lavar los platos', completed: true},
   {text: 'Escribir el reto final de Pyweekend', completed: true},
   {text: 'Terminar el curso de React', completed: false},
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const completed = todos.filter((todo) => !!todo.completed).length;
+  const total = todos.length;
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const searchedTodos = todos.filter((todo) => (
+    todo.text.toLowerCase().includes(searchValue.toLowerCase())) );
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
   return (
     <>
-      <TodoCounter completed={3} total={4}/>
-      <TodoSearch />
+      <TodoCounter completed={completed} total={total}/>
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {defaultTodos.map(todo =>(
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        {searchedTodos.map(todo =>(
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({
+  searchValue,
+  setSearchValue,
+}) {
+  
+
   return (
     <input
       placeholder="Busca tareas"
       className="TodoSearch"
+      value={searchValue}
       onChange={
         (event) => {
-            console.log(event.target.value);
+            setSearchValue(event.target.value);
         }
       }
     />
